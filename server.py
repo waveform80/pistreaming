0f676569290807cece136c36baaa38c93a39bfb6
Dave Jones <dave@waveform.org.uk>
2018-03-01 12:13:03 +0000

    Fix #11, close #21
    A simple fix for iOS (hopefully! I can't test it myself): use HTTP/1.1
    as the version for the websocket response. Also changed avconv to ffmpeg
    since Debian's now gone back to that (and it appears avconv isn't
    present in Stretch).

--- server.py
+++ server.py
@@ -14,7 +14,11 @@ from wsgiref.simple_server import make_server
 
 import picamera
 from ws4py.websocket import WebSocket
-from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
+from ws4py.server.wsgirefserver import (
+    WSGIServer,
+    WebSocketWSGIHandler,
+    WebSocketWSGIRequestHandler,
+)
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
 
 ###########################################
@@ -85,7 +89,7 @@ class BroadcastOutput(object):
     def __init__(self, camera):
         print('Spawning background conversion process')
         self.converter = Popen([
-            'avconv',
+            'ffmpeg',
             '-f', 'rawvideo',
             '-pix_fmt', 'yuv420p',
             '-s', '%dx%d' % camera.resolution,
@@ -134,6 +138,7 @@ def main():
         camera.hflip = HFLIP # flips image left-right, as needed
         sleep(1) # camera warm-up time
         print('Initializing websockets server on port %d' % WS_PORT)
+        WebSocketWSGIHandler.http_version = '1.1'
         websocket_server = make_server(
             '', WS_PORT,
             server_class=WSGIServer,
